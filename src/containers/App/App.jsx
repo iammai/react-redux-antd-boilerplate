c7f8807c3b79f9d22d04c35f96dbde09fafd81c7
Mailan Reiser <mailan.reiser@gmail.com>
2018-03-27 17:03:09 -0700

    Standard layout

--- src/containers/App/App.jsx
+++ src/containers/App/App.jsx
@@ -5,71 +5,33 @@ import { Layout, Menu, Breadcrumb, Icon } from 'antd';
 const { Header, Content, Footer, Sider } = Layout;
 const SubMenu = Menu.SubMenu;
 
-class App extends React.Component {
-  state = {
-    collapsed: false,
-  };
-  onCollapse = (collapsed) => {
-    console.log(collapsed);
-    this.setState({ collapsed });
-  };
-  render() {
-    return (
-      <Layout style={{minHeight: '100vh'}}>
-        <Sider
-          collapsible
-          collapsed={this.state.collapsed}
-          onCollapse={this.onCollapse}
-        >
-          <div className="logo"/>
-          <Menu theme="dark" defaultSelectedKeys={['1']} mode="inline">
-            <Menu.Item key="1">
-              <Icon type="pie-chart"/>
-              <span>Option 1</span>
-            </Menu.Item>
-            <Menu.Item key="2">
-              <Icon type="desktop"/>
-              <span>Option 2</span>
-            </Menu.Item>
-            <SubMenu
-              key="sub1"
-              title={<span><Icon type="user"/><span>User</span></span>}
-            >
-              <Menu.Item key="3">Tom</Menu.Item>
-              <Menu.Item key="4">Bill</Menu.Item>
-              <Menu.Item key="5">Alex</Menu.Item>
-            </SubMenu>
-            <SubMenu
-              key="sub2"
-              title={<span><Icon type="team"/><span>Team</span></span>}
-            >
-              <Menu.Item key="6">Team 1</Menu.Item>
-              <Menu.Item key="8">Team 2</Menu.Item>
-            </SubMenu>
-            <Menu.Item key="9">
-              <Icon type="file"/>
-              <span>File</span>
-            </Menu.Item>
-          </Menu>
-        </Sider>
-        <Layout>
-          <Header style={{background: '#fff', padding: 0}}/>
-          <Content style={{margin: '0 16px'}}>
-            <Breadcrumb style={{margin: '16px 0'}}>
-              <Breadcrumb.Item>User</Breadcrumb.Item>
-              <Breadcrumb.Item>Bill</Breadcrumb.Item>
-            </Breadcrumb>
-            <div style={{padding: 24, background: '#fff', minHeight: 360}}>
-              Bill is a cat.
-            </div>
-          </Content>
-          <Footer style={{textAlign: 'center'}}>
-            Ant Design ©2016 Created by Ant UED
-          </Footer>
-        </Layout>
-      </Layout>
-    );
-  }
-}
+const App = () => (
+  <Layout className="layout">
+    <Header>
+      <div className="logo"/>
+      <Menu
+        theme="dark"
+        mode="horizontal"
+        defaultSelectedKeys={['2']} åå
+        style={{lineHeight: '64px'}}
+      >
+        <Menu.Item key="1">nav 1</Menu.Item>
+        <Menu.Item key="2">nav 2</Menu.Item>
+        <Menu.Item key="3">nav 3</Menu.Item>
+      </Menu>
+    </Header>
+    <Content style={{padding: '0 50px'}}>
+      <Breadcrumb style={{margin: '16px 0'}}>
+        <Breadcrumb.Item>Home</Breadcrumb.Item>
+        <Breadcrumb.Item>List</Breadcrumb.Item>
+        <Breadcrumb.Item>App</Breadcrumb.Item>
+      </Breadcrumb>
+      <div style={{background: '#fff', padding: 24, minHeight: 280}}>Content</div>
+    </Content>
+    <Footer style={{textAlign: 'center'}}>
+      Ant Design ©2016 Created by Ant UED
+    </Footer>
+  </Layout>
+);
 
 export default App;
