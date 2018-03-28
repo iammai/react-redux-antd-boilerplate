de5fb8aa66331ef81f7104d188fd58b2b186e219
Mailan Reiser <1194243+iammai@users.noreply.github.com>
2018-03-27 21:15:31 -0700

    React Router - Add breadcrumbs (#3)
    * users breadcrumb example
    
    * breadcrumb works but it is ugly and limited to the route naming - need names
    
    * breadcrumb example with antd with router 4
    
    * Add breadcrumb header
    
    * Split off into components and constants and import into App

--- src/containers/App/App.jsx
+++ src/containers/App/App.jsx
@@ -1,14 +1,28 @@
 import React from 'react';
-import { Route, Link } from 'react-router-dom';
+import { HashRouter as Router, Route, Switch, Link, withRouter } from 'react-router-dom';
 
-import { Layout, Menu } from 'antd';
+import { Alert, Breadcrumb, Layout, Menu } from 'antd';
 
 import Home from '..//Home/Home';
 import About from '../About/About';
+import BreadcrumbHeader from "../../components/BreadcrumbHeader";
 
 const { Header, Content, Footer } = Layout;
 
-const App = ({ routes, params, children }) => (
+const Users = () => (
+  <div>
+    <ul className="users">
+      <li>
+        <Link to="/users/1">User1</Link>：<Link to="/users/1/detail">Detail</Link>
+      </li>
+      <li>
+        <Link to="/users/2">User2</Link>：<Link to="/users/2/detail">Detail</Link>
+      </li>
+    </ul>
+  </div>
+);
+
+const App = () => (
   <Layout className="layout">
     <Header>
       <div className="logo"/>
@@ -19,13 +33,16 @@ const App = ({ routes, params, children }) => (
         style={{lineHeight: '64px'}}
       >
         <Menu.Item key="1"><Link to="/">Home</Link></Menu.Item>
-        <Menu.Item key="2"><Link to="/about-us">About</Link></Menu.Item>
+        <Menu.Item key="2"><Link to="/users">Users</Link></Menu.Item>
+        <Menu.Item key="3"><Link to="/about-us">About</Link></Menu.Item>
       </Menu>
     </Header>
     <Content style={{padding: '0 50px'}}>
       <div style={{background: '#fff', padding: 24, minHeight: 280}}>
+        <BreadcrumbHeader></BreadcrumbHeader>
         <main>
           <Route exact path="/" component={Home} />
+          <Route exact path="/users" component={Users} />
           <Route exact path="/about-us" component={About} />
         </main>
       </div>
