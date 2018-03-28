827fca12c59dac89c6a540404b47ca43fa176d9a
Mailan Reiser <mailan.reiser@gmail.com>
2018-03-27 18:27:39 -0700

    users breadcrumb example

--- src/containers/App/App.jsx
+++ src/containers/App/App.jsx
@@ -1,14 +1,37 @@
 import React from 'react';
-import { Route, Link } from 'react-router-dom';
+import { Route, Link, history } from 'react-router-dom';
 
-import { Layout, Menu } from 'antd';
+import { Alert, Breadcrumb, Layout, Menu } from 'antd';
 
-import Home from '..//Home/Home';
+// import Home from '..//Home/Home';
 import About from '../About/About';
 
 const { Header, Content, Footer } = Layout;
 
-const App = ({ routes, params, children }) => (
+const Users = () => (
+  <ul className="users">
+    <li>
+      <Link to="/users/1">User1</Link>：<Link to="/users/1/detail">Users</Link>
+    </li>
+    <li>
+      <Link to="/users/2">User2</Link>：<Link to="/users/2/detail">Users</Link>
+    </li>
+  </ul>
+);
+
+const Home = ({ routes, params, children }) => (
+  <div className="home-demo">
+    <div className="demo-nav">
+      <Link to="/">Home</Link>
+      <Link to="/users">Users</Link>
+    </div>
+    {'Home Page'}
+    <Alert style={{ margin: '16px 0' }} message="Click the navigation above to switch:" />
+    <Breadcrumb routes={routes} params={params} />
+  </div>
+);
+
+const App = () => (
   <Layout className="layout">
     <Header>
       <div className="logo"/>
@@ -19,13 +42,15 @@ const App = ({ routes, params, children }) => (
         style={{lineHeight: '64px'}}
       >
         <Menu.Item key="1"><Link to="/">Home</Link></Menu.Item>
-        <Menu.Item key="2"><Link to="/about-us">About</Link></Menu.Item>
+        <Menu.Item key="2"><Link to="/users">Users</Link></Menu.Item>
+        <Menu.Item key="3"><Link to="/about-us">About</Link></Menu.Item>
       </Menu>
     </Header>
     <Content style={{padding: '0 50px'}}>
       <div style={{background: '#fff', padding: 24, minHeight: 280}}>
         <main>
           <Route exact path="/" component={Home} />
+          <Route exact path="/users" component={Users} />
           <Route exact path="/about-us" component={About} />
         </main>
       </div>
